94f0e0961dd65e7bdaa97fdd1da9a2462dcfbb9d
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-09-26 22:32:07 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i--)
+		cout << i << " ";
+	cout << "\n";
+}
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--)
+		solve();
+}
